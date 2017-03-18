a33606af768b6a58a6064c407b1123f3696ca23f
Sean McCann <smccann@zynga.com>
2017-03-17 19:32:56 -0700

    Add Slick carousel to the project
    Still has an issue with 'ajax-loader.gif' not loading properly

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+
 export class App {
   constructor() {
     this.message = 'Hello World!';
+  }
+
+  attached() {
+    $('.slides').slick({
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
   }
 }
